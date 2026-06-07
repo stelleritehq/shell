4886a68c2764bea899d51641372b1f669e5fda2b
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-06-07 18:36:47 +0530

    init: `cargo init`

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+//! Copyright (c) 2026 @rokybeast (Mohammed Sajid Shaik); All Rights Reserved.
+//! The Stellerite Shell (strl) Project is licensed under the Apache 2.0 License.
+//! Slint's Material Design Kit (MD3) is forked by this oraganization, and may be modified
+    //! Original Authors: @tronical, @ogoffart and @NigelBreslaw
+
+fn main() {
+    println!("Hello, Stellerite!");
+}
